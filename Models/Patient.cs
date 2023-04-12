f2cb60eaaf5719ee2082eabd3e54f075381d01e1
tw1ce13 <61151541+tw1ce13@users.noreply.github.com>
2023-04-12 16:38:34 +0500

    Update Patient.cs

--- Models/Patient.cs
+++ Models/Patient.cs
@@ -5,17 +5,17 @@ namespace ProjectJunior.Models
 	public class Patient
 	{
 		public int Id { get; set; }
-        [Required]
-        public string? Name { get; set; }
-        [Required]
-        public string? Surname { get; set; }
-        [Required]
-        public int Age { get; set; }
-        [Required]
-        public string? MainDiagnosis { get; set; }
+        	[Required]
+        	public string? Name { get; set; }
+        	[Required]
+        	public string? Surname { get; set; }
+        	[Required]
+        	public int Age { get; set; }
+        	[Required]
+        	public string? MainDiagnosis { get; set; }
 		public string? SubDiagnosis { get; set; }
-        [Required]
-        public bool IsPrivilege { get; set; }
+        	[Required]
+        	public bool IsPrivilege { get; set; }
 	}
 }
 
