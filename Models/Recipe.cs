8b74354b1cc94ecd47ab130ba02aeba1fbf62504
tw1ce13 <61151541+tw1ce13@users.noreply.github.com>
2023-04-12 16:39:27 +0500

    Update Recipe.cs

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -5,8 +5,8 @@ namespace ProjectJunior.Models
 	public class Recipe
 	{
 		public int Id { get; set; }
-        [Required]
-        public string? Name { get; set; }
+        	[Required]
+        	public string? Name { get; set; }
 		
 	}
 }
