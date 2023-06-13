7914ee2097289ffd639f477fa0811d43b4c206c5
tw1ce13 <61151541+tw1ce13@users.noreply.github.com>
2023-06-13 15:22:50 +0500

    Add files via upload

---
+++ PharmacyProject.Domain/Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace PharmacyProject.Domain.Models
+{
+	public class Patient
+	{
+		public int Id { get; set; }
+		public string? Name { get; set; }
+		public string? Surname { get; set; }
+		public int Age { get; set; }
+		public string? MainDiagnosis { get; set; }
+		public string? SubDiagnosis { get; set; }
+		public bool IsPrivilege { get; set; }
+		public string? Email {get;set;}
+		public string? Password { get; set; }
+	}
+}
+
