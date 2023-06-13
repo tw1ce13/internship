217fbc81fbce4d283dbd87f4976545687dfd96a1
tw1ce13 <61151541+tw1ce13@users.noreply.github.com>
2023-06-13 16:15:21 +0500

    Update PharmacyRepository.cs

--- PharmacyProject.DAL/Repositories/PharmacyRepository.cs
+++ PharmacyProject.DAL/Repositories/PharmacyRepository.cs
@@ -12,41 +12,41 @@ namespace PharmacyProject.DAL.Repositories
 			_context = context;
 		}
 
-        public void Add(Pharmacy data)
-        {
-            _context.Pharmacies.Add(data);
-        }
+		public void Add(Pharmacy data)
+		{
+		    _context.Pharmacies.Add(data);
+		}
 
-        public void Delete(Pharmacy data)
-        {
-            _context.Pharmacies.Remove(data);
-        }
+		public void Delete(Pharmacy data)
+		{
+		    _context.Pharmacies.Remove(data);
+		}
 
 
-        public async Task<IEnumerable<Pharmacy>> GetAll()
-        {
-            var list = await _context.Pharmacies.ToListAsync();
-            return list;
-        }
+		public async Task<IEnumerable<Pharmacy>> GetAll()
+		{
+		    var list = await _context.Pharmacies.ToListAsync();
+		    return list;
+		}
 
-        public async Task<Pharmacy> GetById(int id)
-        {
-            var obj = await _context.Pharmacies.FindAsync(id);
-            return obj!;
-        }
+		public async Task<Pharmacy> GetById(int id)
+		{
+		    var obj = await _context.Pharmacies.FindAsync(id);
+		    return obj!;
+		}
 
-        public async Task<Pharmacy> GetbyName(string name)
-        {
-            var obj = await _context.Pharmacies.FindAsync(name);
-            return obj!;
-        }
+		public async Task<Pharmacy> GetbyName(string name)
+		{
+		    var obj = await _context.Pharmacies.FindAsync(name);
+		    return obj!;
+		}
 
-        public async Task Update(Pharmacy data)
-        {
-            if (data != null)
-                _context.Pharmacies.Update(data);
-            await _context.SaveChangesAsync();
-        }
+		public async Task Update(Pharmacy data)
+		{
+		    if (data != null)
+			_context.Pharmacies.Update(data);
+		    await _context.SaveChangesAsync();
+		}
     }
 }
 
