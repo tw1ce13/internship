d3b82773913e30f2287ff299856476b0e905af85
tw1ce13 <61151541+tw1ce13@users.noreply.github.com>
2023-06-13 15:31:06 +0500

    Update DrugRepository.cs

--- PharmacyProject.DAL/Repositories/DrugRepository.cs
+++ PharmacyProject.DAL/Repositories/DrugRepository.cs
@@ -7,67 +7,67 @@ namespace PharmacyProject.DAL.Repositories
 {
 	public class DrugRepository : IDrugRepository
 	{
-        private readonly PharmacyContext _context;
+        	private readonly PharmacyContext _context;
 		public DrugRepository(PharmacyContext context)
 		{
-            _context = context;
+            		_context = context;
 		}
 
-        public void Add(Drug data)
-        {
-            _context.Drugs.Add(data);
-        }
+		public void Add(Drug data)
+		{
+		    _context.Drugs.Add(data);
+		}
 
-        public void Delete(Drug data)
-        {
-            _context.Drugs.Remove(data);
-        }
+		public void Delete(Drug data)
+		{
+		    _context.Drugs.Remove(data);
+		}
 
 
-        public async Task<IEnumerable<Drug>> GetAll()
-        {
-            var list = await _context.Drugs.ToListAsync();
-            return list;
-        }
+		public async Task<IEnumerable<Drug>> GetAll()
+		{
+		    var list = await _context.Drugs.ToListAsync();
+		    return list;
+		}
 
-        public async Task<Drug> GetById(int id)
-        {
-            var obj = await _context.Drugs.FindAsync(id);
-            return obj!;
-        }
+		public async Task<Drug> GetById(int id)
+		{
+		    var obj = await _context.Drugs.FindAsync(id);
+		    return obj!;
+		}
 
-        public async Task<Drug> GetbyName(string name)
-        {
-            var obj = await _context.Drugs.FindAsync(name);
-            return obj!;
-        }
+		public async Task<Drug> GetbyName(string name)
+		{
+		    var obj = await _context.Drugs.FindAsync(name);
+		    return obj!;
+		}
 
-        public async Task<IEnumerable<DrugResult>> GetDrugs(IEnumerable<Availability> availabilities, IEnumerable<Class> classes, IEnumerable<Delivery> deliveries)
-        {
-            var drugs = await _context.Drugs.ToListAsync();
-            var result = from drug in drugs
-                         join availability in availabilities on drug.Id equals availability.DrugId
-                         join obj in classes on drug.ClassId equals obj.ClassId
-                         join delivery in deliveries on availability.DeliveryId equals delivery.Id
-                         select new DrugResult
-                         {
-                             Id = drug.Id,
-                             Name = drug.Name,
-                             IsRecipe = drug.IsRecipe,
-                             Cost = drug.Cost,
-                             Count = availability.Count,
-                             Type = obj.Type,
-                             ExpirationData = delivery.ExpirationData
-                         };
-            return result;
-        }
+		public async Task<IEnumerable<DrugResult>> GetDrugs(IEnumerable<Availability> availabilities, IEnumerable<Class> classes, IEnumerable<Delivery> deliveries)
+		{
+		    var drugs = await _context.Drugs.ToListAsync();
+		    var result = from drug in drugs
+				 join availability in availabilities on drug.Id equals availability.DrugId
+				 join obj in classes on drug.ClassId equals obj.ClassId
+				 join delivery in deliveries on availability.DeliveryId equals delivery.Id
+				 select new DrugResult
+				 {
+				     Id = drug.Id,
+				     Name = drug.Name,
+				     IsRecipe = drug.IsRecipe,
+				     Cost = drug.Cost,
+				     Count = availability.Count,
+				     Type = obj.Type,
+				     ExpirationData = delivery.ExpirationData
+				 };
+		    return result;
+		}
 
-        public async Task Update(Drug data)
-        {
-            if (data != null)
-                _context.Drugs.Update(data);
-            await _context.SaveChangesAsync();
-        }
+		public async Task Update(Drug data)
+		{
+		    if (data != null)
+			_context.Drugs.Update(data);
+		    await _context.SaveChangesAsync();
+		}
     }
 }
 
