1408c87b71d2f4d4b626972f8088c9d722dfe1eb
tw1ce13 <61151541+tw1ce13@users.noreply.github.com>
2023-06-13 15:28:19 +0500

    Update Availability.cs

--- PharmacyProject.Domain/Models/Availability.cs
+++ PharmacyProject.Domain/Models/Availability.cs
@@ -10,13 +10,13 @@ namespace PharmacyProject.Domain.Models
 	public class Availability
 	{
 		public int Id { get; set; }
-        public int PharmacyId { get; set; }
-        [ForeignKey("PharmacyId")]
-        public Pharmacy? Pharmacy { get; set; }
-        public int DrugId { get; set; }
-        [ForeignKey("DrugId")]
-        public Drug? Drug { get; set; }
-        public int DeliveryId { get; set; }
+		public int PharmacyId { get; set; }
+		[ForeignKey("PharmacyId")]
+		public Pharmacy? Pharmacy { get; set; }
+		public int DrugId { get; set; }
+		[ForeignKey("DrugId")]
+		public Drug? Drug { get; set; }
+		public int DeliveryId { get; set; }
 		[ForeignKey("DeliveryId")]
 		public Delivery? Delivery { get; set; }
 		public int Count { get; set; }
