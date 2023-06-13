e3626c9dd8ad5e294042571f18d1d788462dc23d
tw1ce13 <61151541+tw1ce13@users.noreply.github.com>
2023-06-13 15:33:35 +0500

    Update PharmacyContext.cs

--- PharmacyProject.DAL/PharmacyContext.cs
+++ PharmacyProject.DAL/PharmacyContext.cs
@@ -12,25 +12,25 @@ namespace PharmacyProject.DAL
 
 		}
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {        
-            modelBuilder.Entity<Availability>().HasNoKey();
-            modelBuilder.Entity<RecipeDrug>().HasNoKey();
-        }
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{        
+		    modelBuilder.Entity<Availability>().HasNoKey();
+		    modelBuilder.Entity<RecipeDrug>().HasNoKey();
+		}
 
-        public DbSet<Availability> Availabilities { get; set; }
-        public DbSet<Class> Classes { get; set; }
-        public DbSet<Delivery> Deliveries { get; set; }
-        public DbSet<Discount> Discounts { get; set; }
-        public DbSet<Drug> Drugs { get; set; }
-        public DbSet<Employee> Employees { get; set; }
-        public DbSet<OrdDrug> OrdersDrugs { get; set; }
-        public DbSet<Order> Orders { get; set; }
-        public DbSet<Patient> Patients { get; set; }
-        public DbSet<Pharmacy> Pharmacies { get; set; }
-        public DbSet<Recipe> Recipes { get; set; }
-        public DbSet<RecipeDrug> RecipeDrugs { get; set; }
-        public DbSet<Web> Webs { get; set; }
+		public DbSet<Availability> Availabilities { get; set; }
+		public DbSet<Class> Classes { get; set; }
+		public DbSet<Delivery> Deliveries { get; set; }
+		public DbSet<Discount> Discounts { get; set; }
+		public DbSet<Drug> Drugs { get; set; }
+		public DbSet<Employee> Employees { get; set; }
+		public DbSet<OrdDrug> OrdersDrugs { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<Patient> Patients { get; set; }
+		public DbSet<Pharmacy> Pharmacies { get; set; }
+		public DbSet<Recipe> Recipes { get; set; }
+		public DbSet<RecipeDrug> RecipeDrugs { get; set; }
+		public DbSet<Web> Webs { get; set; }
     }
 }
 
