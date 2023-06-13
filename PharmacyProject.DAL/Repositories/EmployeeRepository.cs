16c04de6699ebed0d48c38da8b20c1ef2c451263
tw1ce13 <61151541+tw1ce13@users.noreply.github.com>
2023-06-13 16:14:48 +0500

    Update EmployeeRepository.cs

--- PharmacyProject.DAL/Repositories/EmployeeRepository.cs
+++ PharmacyProject.DAL/Repositories/EmployeeRepository.cs
@@ -13,41 +13,41 @@ namespace PharmacyProject.DAL.Repositories
 			_context = context;
 		}
 
-        public void Add(Employee data)
-        {
-            _context.Employees.Add(data);
-        }
+		public void Add(Employee data)
+		{
+		    _context.Employees.Add(data);
+		}
 
-        public void Delete(Employee data)
-        {
-            _context.Employees.Remove(data);
-        }
+		public void Delete(Employee data)
+		{
+		    _context.Employees.Remove(data);
+		}
 
 
-        public async Task<IEnumerable<Employee>> GetAll()
-        {
-            var list = await _context.Employees.ToListAsync();
-            return list;
-        }
+		public async Task<IEnumerable<Employee>> GetAll()
+		{
+		    var list = await _context.Employees.ToListAsync();
+		    return list;
+		}
 
-        public async Task<Employee> GetById(int id)
-        {
-            var obj = await _context.Employees.FindAsync(id);
-            return obj!;
-        }
+		public async Task<Employee> GetById(int id)
+		{
+		    var obj = await _context.Employees.FindAsync(id);
+		    return obj!;
+		}
 
-        public async Task<Employee> GetbyName(string name)
-        {
-            var obj = await _context.Employees.FindAsync(name);
-            return obj!;
-        }
+		public async Task<Employee> GetbyName(string name)
+		{
+		    var obj = await _context.Employees.FindAsync(name);
+		    return obj!;
+		}
 
-        public async Task Update(Employee data)
-        {
-            if (data != null)
-                _context.Employees.Update(data);
-            await _context.SaveChangesAsync();
-        }
+		public async Task Update(Employee data)
+		{
+		    if (data != null)
+			_context.Employees.Update(data);
+		    await _context.SaveChangesAsync();
+		}
     }
 }
 
