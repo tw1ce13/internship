b63f4a129b6a72e3c715889fa18cfa7374e9eb6d
tw1ce13 <61151541+tw1ce13@users.noreply.github.com>
2023-06-13 16:16:00 +0500

    Update WebRepository.cs

--- PharmacyProject.DAL/Repositories/WebRepository.cs
+++ PharmacyProject.DAL/Repositories/WebRepository.cs
@@ -12,41 +12,41 @@ namespace PharmacyProject.DAL.Repositories
 			_context = context;
 		}
 
-        public void Add(Web data)
-        {
-            _context.Webs.Add(data);
-        }
+		public void Add(Web data)
+		{
+		    _context.Webs.Add(data);
+		}
 
-        public void Delete(Web data)
-        {
-            _context.Webs.Remove(data);
-        }
+		public void Delete(Web data)
+		{
+		    _context.Webs.Remove(data);
+		}
 
 
-        public async Task<IEnumerable<Web>> GetAll()
-        {
-            var list = await _context.Webs.ToListAsync();
-            return list;
-        }
+		public async Task<IEnumerable<Web>> GetAll()
+		{
+		    var list = await _context.Webs.ToListAsync();
+		    return list;
+		}
 
-        public async Task<Web> GetById(int id)
-        {
-            var obj = await _context.Webs.FindAsync(id);
-            return obj!;
-        }
+		public async Task<Web> GetById(int id)
+		{
+		    var obj = await _context.Webs.FindAsync(id);
+		    return obj!;
+		}
 
-        public async Task<Web> GetbyName(string name)
-        {
-            var obj = await _context.Webs.FindAsync(name);
-            return obj!;
-        }
+		public async Task<Web> GetbyName(string name)
+		{
+		    var obj = await _context.Webs.FindAsync(name);
+		    return obj!;
+		}
 
-        public async Task Update(Web data)
-        {
-            if (data != null)
-                _context.Webs.Update(data);
-            await _context.SaveChangesAsync();
-        }
+		public async Task Update(Web data)
+		{
+		    if (data != null)
+			_context.Webs.Update(data);
+		    await _context.SaveChangesAsync();
+		}
     }
 }
 
